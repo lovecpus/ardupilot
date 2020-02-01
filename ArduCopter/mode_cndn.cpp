bb70d4968db133de1d7de390446cdb2a607cc72d
TAEGYOON KIM <lovecpus@naver.com>
2020-02-01 12:33:06 +0900

    fixed: alt of set_position_target

--- ArduCopter/mode_cndn.cpp
+++ ArduCopter/mode_cndn.cpp
@@ -329,6 +329,7 @@ void ModeCNDN::handle_message(const mavlink_message_t &msg)
                 packet.x = 0.0f;
                 packet.y = 0.0f;
                 packet.z = -25.0f;
+                packet.z += cpos.z;
             }
 #endif
             bTargeted = true;
