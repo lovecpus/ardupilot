837aeb7b4877f7e6ba37f9847dc8580d5b71818e
TAEGYOON KIM <lovecpus@naver.com>
2020-02-05 16:19:59 +0900

    fixed: test mission

--- ArduCopter/mode_cndn.cpp
+++ ArduCopter/mode_cndn.cpp
@@ -542,7 +542,7 @@ void ModeCNDN::handle_message(const mavlink_message_t &msg)
             AP::mission()->add_cmd(cmd);
 
             cmd.index = 2;
-            cmd.id = MAV_CMD_USER_1;
+            cmd.id = MAV_CMD_WAYPOINT_USER_1;
             cmd.p1 = 1;
             cmd.content.location = Location(Vector3f(vecRects.front().x, vecRects.front().y, 300.0f));
             AP::mission()->add_cmd(cmd);
