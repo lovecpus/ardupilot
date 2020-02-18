8d50fa5fdcc0dcb72d5971aa9006024e5a033b3a
TAEGYOON KIM <lovecpus@naver.com>
2020-02-19 00:20:47 +0900

    fixed: yaw

--- ArduCopter/mode_cndn.cpp
+++ ArduCopter/mode_cndn.cpp
@@ -249,7 +249,6 @@ void ModeCNDN::run()
             b_position_target = false;
 
             loiter_nav->init_target();
-            auto_yaw.set_fixed_yaw(last_yaw_cd * 0.01f, 0.0f, 0, false);
             pos_control->set_alt_target_to_current_alt();
             loiter_nav->clear_pilot_desired_acceleration();
 
@@ -257,7 +256,7 @@ void ModeCNDN::run()
 
             const Vector3f tpos(vecRects.back().x, vecRects.back().y, _mission_alt_cm.get() * 1.0f);
             wp_nav->set_wp_destination(tpos, false);
-            auto_yaw.set_mode(AUTO_YAW_HOLD);
+            auto_yaw.set_fixed_yaw(last_yaw_cd * 0.01f, 0.0f, 0, false);
         }
     } break;
 
