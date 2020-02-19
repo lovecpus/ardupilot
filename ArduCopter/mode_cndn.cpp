8886958e9957e680e3a1f56dbf427ac89f8bbe06
TAEGYOON KIM <lovecpus@naver.com>
2020-02-19 16:10:47 +0900

    fixed: compile error

--- ArduCopter/mode_cndn.cpp
+++ ArduCopter/mode_cndn.cpp
@@ -260,8 +260,8 @@ void ModeCNDN::init_speed()
     wp_nav->set_speed_up(_spd_up_cmss.get()*1.0f);
     wp_nav->set_speed_down(_spd_dn_cmss.get()*1.0f);
     wp_nav->wp_and_spline_init();
-    pos_control.set_max_accel_xy(_acc_xy_cmss.get()*1.0f);
-    pos_control.calc_leash_length_xy();
+    pos_control->set_max_accel_xy(_acc_xy_cmss.get()*1.0f);
+    pos_control->calc_leash_length_xy();
 }
 
 void ModeCNDN::run()
@@ -369,8 +369,8 @@ void ModeCNDN::run()
             stage = EDGE_FOLLOW;
             // speed control for edge
             init_speed();
-            pos_control.set_max_speed_xy(_spd_eg_cmss.get()*1.0f);
-            pos_control.calc_leash_length_xy();
+            pos_control->set_max_speed_xy(_spd_eg_cmss.get()*1.0f);
+            pos_control->calc_leash_length_xy();
 
             if (!vecPoints.empty())
             {
