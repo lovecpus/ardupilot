ec8d7c85374101d27816026a8f3e42ca5a04d005
TAEGYOON KIM <lovecpus@naver.com>
2020-02-19 18:23:35 +0900

    fixed: use rc value

--- ArduCopter/mode_cndn.cpp
+++ ArduCopter/mode_cndn.cpp
@@ -1052,7 +1052,7 @@ void ModeCNDN::auto_control()
     if (stage == EDGE_FOLLOW)
     {
         float fv = rc().channel(5)->norm_input();
-        float ferrv = _dst_eg_cm.get() * 0.01f - fv * 5.0f;
+        float ferrv = /*_dst_eg_cm.get() * 0.01f - */fv * 5.0f;
         roll_target += ferrv * 100.0f;
 
         live_log("[CNDN] %0.1f/%0.1f=>%0.4f", fv, ferrv, roll_target);
